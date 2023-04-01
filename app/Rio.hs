c97d30c1285064dbafd08bdb07be93e963368abb
redotter <arthemy.ryzhov@gmail.com>
2023-04-01 23:29:52 +0300

    catch exceptions in print

--- app/Rio.hs
+++ app/Rio.hs
@@ -8,6 +8,7 @@ import Data.Foldable (forM_)
 import System.Exit (exitSuccess)
 import System.IO (hFlush, stdout)
 
+import Data.Xml.Tree (XmlTree)
 import Xml.Handle (XmlReaderMonad, getAttr, getPrompt, getTree, moveDown, moveUp)
 
 data UserInput
@@ -21,34 +22,37 @@ parse :: IO UserInput
 parse = do
     line <- getLine
     let cmd = words line
+    let checkCmd required oper = if length cmd < required
+                                 then error "Not enough args"
+                                 else oper
     case head cmd of
         "print" -> return IPrint
         "up"    -> return IUp
-        "down"  -> getDown cmd
-        "attr"  -> getAttr cmd
+        "down"  -> checkCmd 2 $ return $ IDown $ read $ cmd !! 1
+        "attr"  -> checkCmd 2 $ return $ IAttr $ cmd !! 1
         "exit"  -> return IExit
         _       -> error "Unknown command"
-    where
-        getDown cmd = let index = read $ cmd !! 1
-                      in if index >= 0
-                         then return $ IDown index
-                         else error "Negative index"
-        getAttr cmd = let attr = cmd !! 1
-                      in if (not . null) attr
-                         then return $ IAttr attr
-                         else error "No attribute name"
 
 tryParse :: IO (Maybe UserInput)
 tryParse = do
-    input <- liftIO (try parse :: IO (Either SomeException UserInput))
-    case input of
+    tryResult <- liftIO (try parse :: IO (Either SomeException UserInput))
+    case tryResult of
         Left err    -> putStrLn ("Error: " ++ show err) >> return Nothing
-        Right value -> return $ Just value
+        Right input -> return $ Just input
+
+tryPrint :: XmlReaderMonad ()
+tryPrint = do
+    tree <- getTree
+    let function = print tree
+    tryResult <- liftIO (try function :: IO (Either SomeException ()))
+    case tryResult of
+        Left err -> liftIO $ putStrLn ("Error: " ++ show err)
+        Right _  -> return ()
 
 execute :: UserInput -> XmlReaderMonad ()
-execute IPrint        = getTree >>= (liftIO . print)
-execute IUp           = moveUp >> execute IPrint
-execute (IDown index) = moveDown index >> execute IPrint
+execute IPrint        = tryPrint
+execute IUp           = moveUp >> tryPrint
+execute (IDown index) = moveDown index >> tryPrint
 execute (IAttr attr)  = getAttr attr >>= (liftIO . putStrLn . unwords)
 execute IExit         = liftIO exitSuccess
 
